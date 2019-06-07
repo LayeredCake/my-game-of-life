6d3f11ab0cd274cfab1f3e12723294d640534375
LayeredCake <51462352+LayeredCake@users.noreply.github.com>
2019-06-07 15:59:58 -0400

    Update GridView.java

--- GridView.java
+++ GridView.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.view.View;
 
+import java.util.Random;
+
 /**
  * The main view for MyGameOfLife.
  * Draws a grid of Cells.
@@ -24,12 +26,22 @@ public class GridView extends View {
     /**
      * The width of the grid.
      */
-    int w = 20;
+    int w = 24;
 
     /**
      * The height of the grid
      */
-    int h = 20;
+    int h = 34;
+
+    //0.25: fades to white
+    //0.26: fades to white
+    //0.265: fades to white
+    //0.27: fades to black
+    //0.275: fades to black
+    //0.3: fades to black
+    //0.325: fades to black
+    //0.35: checkers and lines
+    double k = 0.265;
 
     /**
      * The size of a single Cell on the screen.
@@ -42,11 +54,13 @@ public class GridView extends View {
         //Creates new grid.
         grid = new Cell[h][w];
 
+        Random random = new Random();
+
         for(int y = 0; y < h; y++){
             for(int x = 0; x < w; x++){
 
                 //Initializes each Cell in the grid.
-                grid[y][x] = new Cell((x + y) / 8.0);
+                grid[y][x] = new Cell(random.nextInt(6));
 
             }
         }
@@ -64,13 +78,14 @@ public class GridView extends View {
                 canvas.drawRect(x * this.size, y * this.size, (x + 1) * this.size, (y + 1) * this.size, paint);
             }
         }
-        
+
+
         updateGrid();
-        
+
         try{
-            Thread.sleep(30);
+            Thread.sleep(100);
         } catch (InterruptedException e) {}
-        
+
         invalidate();
     }
 
@@ -79,9 +94,59 @@ public class GridView extends View {
         xMax = w - 1;
         yMax = h - 1;
     }
-    
+
     public void updateGrid(){
-        
+
+        double[][] newGrid = new double[h][w];
+        int neighbors;
+        double newEnergy = 0.0;
+
+
+        for(int y =0; y < h; y++){
+            for(int x = 0; x < w; x++){
+
+                neighbors = 4;
+
+                if(x <= 0){
+                    neighbors--;
+                }else{
+                    newEnergy += grid[y][x-1].getEnergy();
+                }
+
+                if(x >= w - 1){
+                    neighbors--;
+                }else{
+                    newEnergy += grid[y][x+1].getEnergy();
+                }
+
+                if(y <= 0){
+                    neighbors--;
+                }else{
+                    newEnergy += grid[y-1][x].getEnergy();
+                }
+
+                if(y >= h - 1){
+                    neighbors--;
+                }else{
+                    newEnergy += grid[y+1][x].getEnergy();
+                }
+
+                if(neighbors == 0){
+                    newEnergy = grid[y][x].getEnergy();
+                }else{
+                    newEnergy = newEnergy / neighbors - k * grid[y][x].getEnergy();
+                }
+
+                newGrid[y][x] = newEnergy;
+
+            }
+        }
+
+        for(int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                grid[y][x].setEnergy(newGrid[y][x]);
+            }
+        }
+
     }
-    
 }
